e74352d9c946456e8e6b00ccd31a46750e8f04c8
Hedon-dev <hedon-dev@hedon-haven.top>
2026-07-04 15:36:39 +0200

    feat: add overrides functionality
    Override the generic banner with version-specific banner if it exists

--- lib/services/banner_manager.dart
+++ lib/services/banner_manager.dart
@@ -1,11 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hedon_haven/ui/utils/toast_notification.dart';
 import 'package:yaml/yaml.dart';
 
 import '/utils/global_vars.dart';
 
-Future<({String title, String message})> getBanner() async {
-  final yamlRaw = (await client
-          .get(Uri.parse("https://banner.hedon-haven.top/banner.yaml")))
-      .body;
-  YamlMap yaml = loadYaml(yamlRaw);
-  return (title: yaml["title"] as String, message: yaml["message"] as String);
+Future<({String severity, String title, String message})?> getBanner(
+    BuildContext context) async {
+  try {
+    final yamlRaw = (await client
+            .get(Uri.parse("https://banner.hedon-haven.top/banner.yaml")))
+        .body;
+    YamlMap yaml = loadYaml(yamlRaw);
+
+    // Show version specific message if it exists
+    YamlMap? override = yaml['overrides']?[packageInfo.version];
+    var bannerRecord = (
+      severity: (override?["severity"] ?? yaml["severity"]) as String,
+      title: (override?["title"] ?? yaml["title"]) as String,
+      message: (override?["message"] ?? yaml["message"]) as String
+    );
+    logger.i("Displaying banner with: $bannerRecord");
+    return bannerRecord;
+  } catch (e) {
+    logger.e("Error getting banner: $e");
+    if (context.mounted) showToast("Error getting banner: $e", context);
+    return null;
+  }
 }
