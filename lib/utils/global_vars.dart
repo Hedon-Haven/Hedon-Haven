f2d1534d308af6e14c9ff3247cb0b929052266dc
Hedon-dev <hedon-dev@hedon-haven.top>
2026-07-09 21:13:16 +0200

    chore: formatting

--- lib/utils/global_vars.dart
+++ lib/utils/global_vars.dart
@@ -19,10 +19,12 @@ String httpUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36";
 // Generic hint headers from a Windows Chrome Browser
 Map<String, String> defaultHttpHeaders = {
-  "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  "accept":
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
   "accept-language": 'en-US,en;q=0.9',
   "priority": 'u=0, i',
-  "sec-ch-ua": '"Google Chrome";v="149", "Chromium";v="149", "Not)A;Brand";v="24"',
+  "sec-ch-ua":
+      '"Google Chrome";v="149", "Chromium";v="149", "Not)A;Brand";v="24"',
   "sec-ch-ua-mobile": '?0',
   "sec-ch-ua-platform": '"Windows"',
   "sec-fetch-dest": 'document',
