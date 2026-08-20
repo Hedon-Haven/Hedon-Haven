fa93ca3f38a3209d481b622d252ffe1268032836
Hedon-dev <hedon-dev@hedon-haven.top>
2026-08-20 18:18:19 +0200

    feat: rename shared_prefs_manager_upgrades to upgrade_manager

--- lib/services/shared_prefs_manager.dart
+++ lib/services/shared_prefs_manager.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '/services/database_manager.dart';
-import '/services/shared_prefs_manager_upgrades.dart';
+import '/services/upgrade_manager.dart';
 import '/utils/global_vars.dart';
 
 Future<void> setDefaultSettings([bool forceReset = false]) async {
