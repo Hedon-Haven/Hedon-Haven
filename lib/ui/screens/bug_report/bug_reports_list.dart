568366a9ebc5e11650d2ce6e05b5228e355eacfb
Hedon-dev <hedon-dev@hedon-haven.top>
2026-08-23 21:01:31 +0200

    feat: Add scraping report mode to bug reports list

--- lib/ui/screens/bug_report/bug_reports_list.dart
+++ lib/ui/screens/bug_report/bug_reports_list.dart
@@ -1,16 +1,22 @@
 import 'dart:convert';
 
+import 'package:hedon_haven/ui/utils/toast_notification.dart';
 import 'package:material_ui/material_ui.dart';
 
 import '/services/bug_report_manager.dart';
 import '/services/plugin_manager.dart';
 import '/utils/exceptions.dart';
+import '/utils/global_vars.dart';
 import '/utils/plugin_interface/plugin_interface.dart';
+import 'bug_report.dart';
 
 class BugReportsListScreen extends StatefulWidget {
   final List<BugReport> bugReportsList;
+  final bool scrapingReportMode;
 
-  const BugReportsListScreen({super.key, required this.bugReportsList});
+  const BugReportsListScreen(
+      {super.key, required this.bugReportsList, bool? scrapingReportMode})
+      : scrapingReportMode = scrapingReportMode ?? false;
 
   @override
   State<BugReportsListScreen> createState() => _BugReportsListScreenState();
@@ -19,6 +25,7 @@ class BugReportsListScreen extends StatefulWidget {
 class _BugReportsListScreenState extends State<BugReportsListScreen> {
   /// Concatenated list of all bug reports
   late Map<String, List<BugReport>> sortedBugReports;
+  bool enableReportButton = false;
 
   @override
   void initState() {
@@ -30,6 +37,13 @@ class _BugReportsListScreenState extends State<BugReportsListScreen> {
       ...?groupedReports.bundledPluginGroups,
       ...?groupedReports.thirdPartyPluginGroups
     };
+
+    if (widget.scrapingReportMode) {
+      // Enable report button only if non-CustomExceptions are in the list
+      enableReportButton =
+          widget.bugReportsList.any((r) => r.exception is! CustomException);
+      logger.i("Bug report button enabled: $enableReportButton");
+    }
   }
 
   @override
@@ -38,28 +52,73 @@ class _BugReportsListScreenState extends State<BugReportsListScreen> {
         appBar: AppBar(
             iconTheme:
                 IconThemeData(color: Theme.of(context).colorScheme.primary),
-            title: Text("Included bug reports",
+            title: Text(
+                widget.scrapingReportMode
+                    ? "Scraping report"
+                    : "Included bug reports",
                 style: Theme.of(context).textTheme.headlineLarge)),
+        floatingActionButtonLocation: widget.scrapingReportMode
+            ? FloatingActionButtonLocation.centerFloat
+            : null,
+        floatingActionButton:
+            widget.scrapingReportMode ? buildFloatingButton() : null,
         body: SafeArea(
             child: ListView(
                 padding: const EdgeInsets.all(8),
-                children: List.generate(
-                    sortedBugReports.isEmpty ? 1 : sortedBugReports.length,
-                    (index) => buildGroupTile(
-                        sortedBugReports.entries.elementAt(index))))));
+                // Display AppReports directly without nesting
+                children: (sortedBugReports.keys.length == 1 &&
+                        sortedBugReports.keys.first == "App bug reports")
+                    ? sortedBugReports.values.first
+                        .map(buildReportTile)
+                        .toList()
+                    : sortedBugReports.entries.map(buildGroupTile).toList())));
+  }
+
+  Widget buildFloatingButton() {
+    return FloatingActionButton.extended(
+      backgroundColor: enableReportButton
+          ? Theme.of(context).colorScheme.primary
+          : Theme.of(context).colorScheme.onSurface.withValues(alpha: 0.12),
+      foregroundColor: enableReportButton
+          ? Theme.of(context).colorScheme.onPrimary
+          : Theme.of(context).colorScheme.onSurface.withValues(alpha: 0.38),
+      // reduce padding around text
+      extendedPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+      label: Text("Create bug report"),
+      onPressed: () async {
+        if (!enableReportButton) {
+          showToast("Nothing to report, all issues were external site problems",
+              context);
+          return;
+        }
+        List<BugReport> submittedReports = await Navigator.push(
+          context,
+          MaterialPageRoute(
+            settings: RouteSettings(name: "/bug-report"),
+            builder: (context) => BugReportScreen(
+                submissionType: SubmissionType.userApproved,
+                bugReportsList: widget.bugReportsList),
+          ),
+        );
+        // Close Scraping report on successful return
+        if (mounted && submittedReports.isNotEmpty) {
+          Navigator.of(context).pop(submittedReports);
+        }
+      },
+    );
   }
 
   Widget buildGroupTile(MapEntry<String, List<BugReport>> bugReportGroup) {
     return FutureBuilder<PluginInterface?>(
         future: PluginManager.getPluginByName(bugReportGroup.key),
         builder: (context, snapshot) {
-          // Simplify display for appExceptions
+          // Simplify display for CustomExceptions
           if (bugReportGroup.value.length == 1 &&
-              bugReportGroup.value.first is ProviderException) {
+              bugReportGroup.value.first.exception is CustomException) {
             String titleText =
                 "${snapshot.data?.prettyName ?? bugReportGroup.key}"
                 ": "
-                "${(bugReportGroup.value.first.exception as ProviderException).title}";
+                "${(bugReportGroup.value.first.exception as CustomException).title}";
 
             return ListTile(
               title: Text(titleText),
@@ -74,7 +133,7 @@ class _BugReportsListScreenState extends State<BugReportsListScreen> {
             title: Text(bugReportGroup.key == "appReports"
                 ? "App bug reports"
                 : snapshot.data?.prettyName ?? bugReportGroup.key),
-            controlAffinity: ListTileControlAffinity.leading,
+            controlAffinity: ListTileControlAffinity.trailing,
             // Remove white lines
             collapsedShape: RoundedRectangleBorder(),
             shape: RoundedRectangleBorder(),
@@ -89,7 +148,7 @@ class _BugReportsListScreenState extends State<BugReportsListScreen> {
   Widget buildReportTile(BugReport report) {
     return ExpansionTile(
         title: Text(report.exception.toString()),
-        controlAffinity: ListTileControlAffinity.leading,
+        controlAffinity: ListTileControlAffinity.trailing,
         // Remove white lines
         collapsedShape: RoundedRectangleBorder(),
         shape: RoundedRectangleBorder(),
